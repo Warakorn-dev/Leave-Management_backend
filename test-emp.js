e6c3dfd393c84ea7e1dfa0849340f21ccfa1c94c
PATHOMPORN\patho <pathomporn3510@gmail.com>
2026-08-10 14:10:04 +0700

    แก้นิดหน่อย

--- test-emp.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config();
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-prisma.employee.findMany({ select: { id: true, employeeCode: true, firstName: true } }).then(console.log).finally(() => prisma.$disconnect());
